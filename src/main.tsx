436555c701fdf5e190fef3b113f02cc20b16826d
Dibya Barua <enisagrande@gmail.com>
2025-04-13 14:23:56 +0200

    Added Loading Spinner

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,25 @@
-import { StrictMode, lazy } from 'react'
+import { StrictMode, lazy, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Outlet } from "react-router"
 import './index.css'
+import Loading from './pages/Loading.tsx';
 
 const Navbar = lazy(() => import('./Navbar.tsx'));
 const Homepage = lazy(() => import('./pages/Homepage.tsx'));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<>
-          <Navbar />
-          <Outlet />
-        </>}>
-          <Route index element={<Homepage />}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<Loading />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<>
+            <Navbar />
+            <Outlet />
+          </>}>
+            <Route index element={<Homepage />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   </StrictMode>,
 )
